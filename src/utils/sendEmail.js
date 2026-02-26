133e599c75f63b0b8f809130c3fdd0db34b7ccab
AK-GUPTA-20 <guptaakshat7795@gmail.com>
2026-02-26 19:47:36 +0530

    update email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (options) => {
   try {
     await resend.emails.send({
-      from: process.env.RESEND_FROM_EMAIL, 
+      from:"onboarding@resend.dev", 
       to: options.email,
       subject: options.subject,
       html: options.html || `<p>${options.message}</p>`,
